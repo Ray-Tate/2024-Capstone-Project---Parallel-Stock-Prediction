51dee1832db5c989baa8ceec39b0b50767ec11f7
Someone256 <71982556+Someone256@users.noreply.github.com>
2024-11-11 23:53:19 -0500

    Added dividend collection (needs work)

--- data_collection/getData.py
+++ data_collection/getData.py
@@ -1,18 +1,22 @@
 import yfinance as yf
 import pandas as pd
+import yahoo_fin.stock_info as si
 
 
 #close prices
 ticker_symbols = ["CM.TO","RY.TO","BMO.TO","TD.TO","BNS.TO","EQB.TO","NA.TO"]
 
 close_data = pd.DataFrame({})
-
+dividend_data = pd.DataFrame({})
+pe_data = pd.DataFrame({})
 # Fetch historical market data
-for symbol in ticker_symbols :
-    ticker = yf.Ticker(symbol)
-    historical_data = ticker.history(period="2y")  # data for the last year
-    close = historical_data["Close"]
-    close_data[symbol] = close
-print(historical_data)
-print(close_data)
+if True:
+    for symbol in ticker_symbols :
+        ticker = yf.Ticker(symbol)
+        historical_data = ticker.history(period="2y")  
+        close = historical_data["Close"]
+        close_data[symbol] = close
+        dividend_data[symbol] = ticker.get_dividends()
+
+
 
