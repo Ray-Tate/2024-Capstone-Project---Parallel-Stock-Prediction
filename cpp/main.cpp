e5659a4379932868d4994b4deefdbd7c89614ab9
Ray Tate <raytate1234@gmail.com>
2025-01-09 15:58:47 -0500

    Added a stock data class to tell what data came from what stock. Cleaned up main. Read in stock data gets printed fully.

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -4,6 +4,19 @@
 #include <string>
 #include "json.hpp"
 
+nlohmann::json getConfig(){
+    const std::string configPath = "config.json";
+    std::ifstream configFile(configPath);
+    if (!configFile.is_open()) { //If error opening the file
+        std::cerr << "Unable to open file: " + configPath << std::endl;
+        exit(1);
+    }
+    nlohmann::json jsonConfig; // Declare a nlohmann::json object
+    configFile >> jsonConfig; // Parse JSON data from the file
+    configFile.close();
+    std::cout << "Data read from config file '" << configPath << ":'" << jsonConfig.dump(4) << std::endl;
+    return jsonConfig;
+}
 
 std::vector<double> file2arr(const std::string& filePath) {
     std::string readLine;
@@ -21,28 +34,66 @@ std::vector<double> file2arr(const std::string& filePath) {
     }
 
     return array;
-    
 }
+
+class StockData {
+private:
+    std::string name;                // Name of the data holder
+    std::vector<double> doubleArray;   // Array of doubles
+
+public:
+    // Constructor
+    StockData(const std::string name, const std::vector<double>& doubleArray)
+        : name(name), doubleArray(doubleArray) {}
+
+    // Default constructor
+    StockData() : name("Unnamed"), doubleArray({}) {}
+
+    // Getters
+    std::string getName() const {
+        return name;
+    }
+
+    std::vector<double> getDoubleArray() const {
+        return doubleArray;
+    }
+
+    // Setters # We should never need to use these, data should be final after constructor
+    //void setName(const std::string& newName) {
+    //    name = newName;
+    //}
+
+    //void setdoubleArray(const std::vector<double>& newArray) {
+    //    doubleArray = newArray;
+    //}
+
+    // Method to print data
+    void printStockData() const {
+        std::cout << "Name: " << name << "\nArray: ";
+        for (double value : doubleArray) {
+            std::cout << value << " ";
+        }
+        std::cout << std::endl;
+    }
+};
 
 
 int main() {
+    nlohmann::json jsonConfig = getConfig();
     
-    //Reading in config.json file
-    const std::string configPath = "config.json";
-    std::ifstream configFile(configPath);
-    if (!configFile.is_open()) { //If error opening the file
-        std::cerr << "Unable to open file: " + configPath << std::endl;
-        exit(1);
-    }
-    nlohmann::json jsonConfig; // Declare a nlohmann::json object
-    configFile >> jsonConfig; // Parse JSON data from the file
-    configFile.close();
-    std::cout << "Parsed JSON data:\n" << jsonConfig.dump(4) << "\n";
-
+    //Read the config for which stocks to use and read in the respective data text files
+    std::vector<StockData> allStockData;
     for(std::string stock : jsonConfig["STOCKS"]){
-        std::vector<double> arr = file2arr("InputData/"+stock+".txt");
-        std::cout << stock << std::endl;
+        StockData tmp(stock, file2arr("InputData/"+stock+".txt"));
+        allStockData.push_back(tmp);
     }
+
+    //Print all the read in stock data
+    for(StockData stock : allStockData){
+        stock.printStockData();
+    }
+
+    std::cout << "DONE!!!!!!!!\n" << std::endl;
 
     return 0;
 }
