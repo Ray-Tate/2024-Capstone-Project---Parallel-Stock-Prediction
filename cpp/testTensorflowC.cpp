13811d21b4fbc7f7f13f597a877efe495008889f
Ray Tate <raytate1234@gmail.com>
2025-01-12 21:33:52 -0500

    Small clean up.

--- cpp/testTensorflowC.cpp
+++ cpp/testTensorflowC.cpp
@@ -7,10 +7,8 @@
 #include "tensorflow/c/c_api.h"
 
 int main() {
-  //printf("Hello from TensorFlow C library version %s\n", TF_Version());
-  //printf("Hello from TensorFlow C library version.");
   std::cout << "HELLO" << std::endl;
   std::cout << "The tensorflow version is: " << TF_Version() << std::endl;
-  std::cout << "HELLO2" << std::endl;
+  std::cout << "BYE" << std::endl;
   return 0;
 }
